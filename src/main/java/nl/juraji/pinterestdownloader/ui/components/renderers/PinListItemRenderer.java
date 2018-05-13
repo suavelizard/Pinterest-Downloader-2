b1b422d81c5a748f8b75784ce136adbae40293ef
Juraji <robin@juraji.nl>
2018-05-14 00:05:39 +0200

    - PinListItemRenderer.java: Fix issue when hashes are not set the renderer throws a NullPointerException

--- src/main/java/nl/juraji/pinterestdownloader/ui/components/renderers/PinListItemRenderer.java
+++ src/main/java/nl/juraji/pinterestdownloader/ui/components/renderers/PinListItemRenderer.java
@@ -1,6 +1,7 @@
 package nl.juraji.pinterestdownloader.ui.components.renderers;
 
 import nl.juraji.pinterestdownloader.model.Pin;
+import nl.juraji.pinterestdownloader.model.PinImageHash;
 import nl.juraji.pinterestdownloader.resources.I18n;
 import nl.juraji.pinterestdownloader.util.FileUtils;
 
@@ -18,9 +19,14 @@ public class PinListItemRenderer implements ListCellRenderer<Pin> {
     public Component getListCellRendererComponent(JList<? extends Pin> list, Pin pin, int index, boolean isSelected, boolean cellHasFocus) {
         final JLabel label = (JLabel) defaultListCellRenderer.getListCellRendererComponent(list, pin, index, isSelected, cellHasFocus);
 
+        PinImageHash hash = pin.getImageHash();
+        if (hash == null) {
+            hash = new PinImageHash();
+        }
+
         final String imgPath = getPath(pin);
-        final int imageWidth = pin.getImageHash().getImageWidth();
-        final int imageHeight = pin.getImageHash().getImageHeight();
+        final int imageWidth = hash.getImageWidth();
+        final int imageHeight = hash.getImageHeight();
         double ratio = (100.0 / imageWidth);
         int tgtWidth = (int) (imageWidth * ratio);
         int tgtHeight = (int) (imageHeight * ratio);
@@ -32,10 +38,10 @@ public class PinListItemRenderer implements ListCellRenderer<Pin> {
                 pin.getBoard().getName(),
                 imgPath,
                 pin.getPinId(),
-                FileUtils.bytesInHumanReadable(pin.getImageHash().getImageSizeBytes()),
+                FileUtils.bytesInHumanReadable(hash.getImageSizeBytes()),
                 imageWidth,
                 imageHeight,
-                pin.getImageHash().getContrast());
+                hash.getContrast());
 
 
         label.setText(wrapper);
